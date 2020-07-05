8bb1143847c95b408ca9f97bf101e862bf9fe4ad
hunter_out <hunter_out@icloud.com>
2020-07-06 00:26:40 +0300

    добавил класс "вакансии" и переопределил метод сейв для автозаполнение слага, если  он не указан

--- scraping/models.py
+++ scraping/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import from_cyrillic_to_eng
 
 
 class City(models.Model):
@@ -7,19 +8,38 @@ class City(models.Model):
     
     class Meta:
         verbose_name = 'Название населенного пункта'
-        verbose_name_plural = 'Название населенных пунктов'
+        verbose_name_plural = 'Названия населенных пунктов'
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
         
     def __str__(self):
         return self.name
 
 
 class Language(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название населенного пункта')
+    name = models.CharField(max_length=50, verbose_name='Язык програмирования')
     slug = models.CharField(max_length=50, blank=True)
     
     class Meta:
-        verbose_name = 'Название населенного пункта'
-        verbose_name_plural = 'Название населенных пунктов'
+        verbose_name = 'Язык програмирования'
+        verbose_name_plural = 'Языки програмирования'
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
         
     def __str__(self):
         return self.name
+    
+    
+class Vacancy(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=250, verbose_name='')
+    company = models.CharField(max_length=250, verbose_name='')
+    description = models.TextField(verbose_name='')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='')
